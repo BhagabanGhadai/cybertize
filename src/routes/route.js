e69f2e6a529cea4abdc1b4900169da3dc069577d
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2022-12-21 17:07:08 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,11 +5,12 @@ const {signup,login}=require('../controllers/userController')
 const category=require('../controllers/categoryController')
 const subCategory=require('../controllers/subCategoryController')
 const childCategory=require('../controllers/childCategoryControlle')
-
+const getDetails=require('../controllers/getdata')
 router.post('/signup',signup)
 router.post('/login',login)
 router.post('/category',category)
 router.post('/subcategory/:categoryId',subCategory)
 router.post('/childcategory/:subCategoryId',childCategory)
+router.get('/user',getDetails)
 
 module.exports=router
